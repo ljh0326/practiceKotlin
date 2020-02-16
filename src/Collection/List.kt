e5d4b15bfceddc7664e5e4359f3d2685ea7d25e0
이정훈 <hun2@tossInsu.com>
2020-02-16 16:16:15 +0900

    리스트 예제 listOf, mutableListOf

---
+++ src/Collection/List.kt
@@ -0,0 +1,35 @@
+package Collection
+
+/**
+ * List:        생성시 넣은 객체를 대체, 추가, 삭제 불가
+ * MutableList: 생성시 넣은 객체를 대체, 추가, 삭제 가
+ * */
+
+fun main() {
+
+    val a = listOf("사과", "딸기", "배")
+
+    for(fruit in a){
+        print(fruit)
+    }
+
+    println()
+
+    val b = mutableListOf(6, 3, 1)
+    println(b)
+
+    b.add(4)
+    println(b)
+
+    b.add(2, 8)
+    println(b)
+
+    b.removeAt(1)
+    println(b)
+
+    b.shuffle()
+    println(b)
+
+    b.sort()
+    println(b)
+}
